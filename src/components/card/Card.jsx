0e2a2674bb36b57aa0332645cb4c48501d65d7ad
AVenturi98 <lssndrventuri@gmail.com>
2024-12-01 23:50:38 +0100

    ADD: color for each tag

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -3,6 +3,17 @@ import image from '../../assets/31343C.svg'
 
 function Card({ title, content, tags, published }) {
 
+    const elementTags = tags.map((tag, elmnTag, className) => {
+
+        if (tag === 'html') className = 'htmlColor'
+        if (tag === 'css') className = 'cssColor'
+        if (tag === 'js') className = 'jsColor'
+        if (tag === 'php') className = 'phpColor'
+
+
+        return <p key={elmnTag} className={className}>{tag}</p>
+    })
+
     return (
         <>
             {published &&
@@ -13,8 +24,8 @@ function Card({ title, content, tags, published }) {
                         </div>
                         <div className={style.cardBody}>
                             <div className={style.titleCard}>{title}</div>
+                            <div className={style.tagsCard}>{elementTags}</div>
                             <div className={style.descriptionCard}>{content}</div>
-                            <div>{tags}</div>
                             <button className={style.btn}>BUTTON</button>
                         </div>
                     </div>
